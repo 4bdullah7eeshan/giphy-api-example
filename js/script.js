85beaa3a5f5462e16e4dd1f98f4e1818f5971014
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-09-16 00:18:46 +0530

    feat: add search box

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
-const newGif = document.querySelector("#new");
-const img = document.querySelector("img");
+const searchInput = document.querySelector("#search");
+const searchButton = document.querySelector("#search_button");
+const gifContainer = document.querySelector(".gif-container");
 
-newGif.addEventListener('click', () => {
-    window.location.reload();
+const gif = document.createElement("img");
+
+const noGIFsFoundMessage = document.createElement("p");
+noGIFsFoundMessage.textContent = "No GIFs found :(.";
+
+let sParameter, url;
+
+url =
+  "https://api.giphy.com/v1/gifs/translate?api_key=ISqXk4wJHYuq1KZFDkvtcPOIUJfwzRmA&s=";
+
+searchButton.addEventListener("click", () => {
+  if (searchInput.value != "") {
+    sParameter = searchInput.value;
+    fetch(url + sParameter, {
+      mode: "cors",
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        gifContainer.innerHTML = "";
+        gif.src = response.data.images.original.url;
+        gif.alt = sParameter;
+        gifContainer.appendChild(gif);
+      })
+      .catch(function () {
+        gifContainer.appendChild(noGIFsFoundMessage);
+      });
+  }
 });
-
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=ISqXk4wJHYuq1KZFDkvtcPOIUJfwzRmA&s=cats",
-  {
-    mode: "cors",
-  },
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
